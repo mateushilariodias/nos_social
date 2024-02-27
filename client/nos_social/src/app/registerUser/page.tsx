c7ecd6a77f422d30633d45ec10c900a43697236d
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-02-27 15:34:00 -0300

    create user registration page and add fields to the user registration form

---
+++ client/nos_social/src/app/registerUser/page.tsx
@@ -0,0 +1,29 @@
+function registerUser() {
+    return (
+        <main>
+            <form action=""></form>
+            <h1></h1>
+            <div>
+                <label htmlFor="name"></label>
+                <input type="text" name="name" id="name" />
+            </div>
+            <div>
+                <label htmlFor="userName"></label>
+                <input type="text" name="userName" id="userName" />
+            </div>
+            <div>
+                <label htmlFor="emailUser">E-mail</label>
+                <input type="email" name="emailUser" id="emailUser" />
+            </div>
+            <div>
+                <label htmlFor="phoneNumberUser">Celular</label>
+                <input type="tel" name="phoneNumberUser" id="phoneNumberUser" />
+            </div>
+            <div>
+                <label htmlFor="passwordUser">Senha</label>
+                <input type="password" name="passwordUser" id="passwordUser" />
+            </div>
+        </main>
+    );
+}
+export default registerUser;

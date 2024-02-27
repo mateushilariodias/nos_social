1bef77c935dafffbe0a05a11da0e01c5d7d86e77
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-02-27 16:30:39 -0300

    create a user login page and add form fields to enter your account and update role name on user registration page

---
+++ client/nos_social/src/app/loginUser/page.tsx
@@ -0,0 +1,17 @@
+function LoginUser() {
+    return (
+        <main>
+            <form action=""></form>
+            <h1></h1>
+            <div>
+                <label htmlFor="emailUser">E-mail:</label>
+                <input type="email" name="emailUser" id="emailUser" />
+            </div>
+            <div>
+                <label htmlFor="passwordUser">Senha:</label>
+                <input type="password" name="passwordUser" id="passwordUser" />
+            </div>
+        </main>
+    )
+}
+export default LoginUser;
--- client/nos_social/src/app/registerUser/page.tsx
+++ client/nos_social/src/app/registerUser/page.tsx
@@ -1,29 +1,29 @@
-function registerUser() {
+function RegisterUser() {
     return (
         <main>
             <form action=""></form>
             <h1></h1>
             <div>
-                <label htmlFor="name">Nome</label>
+                <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" id="name" />
             </div>
             <div>
-                <label htmlFor="userName">Nome de usuário</label>
+                <label htmlFor="userName">Nome de usuário:</label>
                 <input type="text" name="userName" id="userName" />
             </div>
             <div>
-                <label htmlFor="emailUser">E-mail</label>
+                <label htmlFor="emailUser">E-mail:</label>
                 <input type="email" name="emailUser" id="emailUser" />
             </div>
             <div>
-                <label htmlFor="phoneNumberUser">Celular</label>
+                <label htmlFor="phoneNumberUser">Celular:</label>
                 <input type="tel" name="phoneNumberUser" id="phoneNumberUser" />
             </div>
             <div>
-                <label htmlFor="passwordUser">Senha</label>
+                <label htmlFor="passwordUser">Senha:</label>
                 <input type="password" name="passwordUser" id="passwordUser" />
             </div>
         </main>
     );
 }
-export default registerUser;
+export default RegisterUser;

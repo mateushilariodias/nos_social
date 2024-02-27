21537106c20a72b8d36b76f210dc5c27be0f7dfc
mateushilariodias <mateusilariodias@gmail.com>
2024-02-26 21:17:09 -0300

    first commit

---
+++ client/nos_social/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-purple-700 underline">Stack Front-End</h1>
+    </main>
+  );
+}

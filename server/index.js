8dd9bc0ec09637afa38ea65c9b6ab280074d3deb
Lorena Ribeiro Freitas <lorenarr004@gmail.com>
2024-03-25 17:47:28 -0300

    Add functionality for creating posts

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 // Importação dos módulos necessários do Express.js para criar o aplicativo e configurar rotas.
 import express from "express";
 import bodyParser from "body-parser"; // Body parser é usado para analisar corpos de requisição HTTP em middleware.
-
 // Importação dos roteadores de usuário e autenticação.
 import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
+import postRouter from "./routes/post.js";
 
 // Criação do aplicativo Express.
 const app = express();
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Configuração das rotas para os roteadores de usuário e autenticação.
 app.use("/server/users/", userRouter); // Configurando rota principal dos usuários.
 app.use("/server/auth/", authRouter);   // Configurando rota principal da autenticação.
-
+app.use("/server/post/", postRouter); // Configurar rota principal dos posts
 // Inicialização do servidor na porta 3306.
 app.listen(3306, () => {
     console.log("Servidor rodando na porta 3306!");

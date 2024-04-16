0e26b0576a9fae51fc67ffaf695988acfad6c210
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-04-16 16:57:23 -0300

    cinsere code that handles creating and deleting likes

--- client/nos_social/src/components/Post.tsx
+++ client/nos_social/src/components/Post.tsx
@@ -16,9 +16,11 @@ interface IPost {
     createdPost: string;
 }
 
-interface IUser {
+interface ILike {
+    id: number;
     userName: string;
-    userImg: string;
+    likeUserId: number;
+    postId: number
 }
 
 interface IComment {
@@ -36,10 +38,58 @@ function Post(props: { post: IPost }) {
     const { id, profilePicture, author, description, image, createdPost } = props.post;
     const { user } = useContext(UserContext)
     const [comment, setComment] = useState('')
+    const [liked, setLiked] = useState(false)
+    const [showLiked, setShowLiked] = useState(false)
     const [showComments, setShowComments] = useState(false)
     const queryClient = useQueryClient()
 
-    const { data, error, isLoading } = useQuery<IComment[] | undefined>({
+    //LIKES QUERY
+
+    const likeQuery = useQuery<ILike[] | undefined>({
+        queryKey: ['likes', id],
+        queryFn: () => makeRequest.get('like/?post_id' + id).then((res) => {
+            res.data.data.map((like: ILike) => {
+                if (like.likeUserId === user?.id) {
+                    return setLiked(true);
+                }
+                else {
+                    setLiked(false);
+                }
+            })
+            return res.data.data
+        }),
+        enabled: !!id
+    })
+
+    if (likeQuery.error) {
+        console.log(likeQuery.error);
+    }
+
+    const likeMutation = useMutation({
+        mutationFn: async (newLike: {}) => {
+            if (liked) {
+                await makeRequest.delete(`like/?postId=${id}&likeUserId=${user?.id}`).then((res) => {
+                    setLiked(false)
+                    return res.data;
+                });
+            } else {
+                await makeRequest.post("like/", newLike).then((res) => {
+                    return res.data;
+                });
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["likes", id] })
+        },
+    });
+
+    const shareLike = async () => {
+        likeMutation.mutate({ likeUserId: user?.id, postId: id });
+    };
+
+    //COMMENTS QUERY
+
+    const commentQuery = useQuery<IComment[] | undefined>({
         queryKey: ['comments', id],
         queryFn: () => makeRequest.get('comment/?post_id' + id).then((res) => {
             return res.data.data
@@ -47,11 +97,11 @@ function Post(props: { post: IPost }) {
         enabled: !!id
     })
 
-    if (error) {
-        console.log(error);
+    if (commentQuery.error) {
+        console.log(commentQuery.error);
     }
 
-    const mutation = useMutation({
+    const commentMutation = useMutation({
         mutationFn: async (newComment: {}) => {
             await makeRequest.post("comment/", newComment).then((res) => {
                 return res.data
@@ -63,7 +113,7 @@ function Post(props: { post: IPost }) {
     });
 
     const shareComment = async () => {
-        mutation.mutate({ comment, commentUserId: user?.id, postId: id });
+        commentMutation.mutate({ comment, commentUserId: user?.id, postId: id });
         setComment('')
     }
 
@@ -84,22 +134,38 @@ function Post(props: { post: IPost }) {
             </main>
             <footer>
                 <div className="flex justify-between py-4 border-b">
-                    <div className="flex gap-1 items-center">
-                        <span className="bg-blue-600 w-6 h-6 text-white flex items-center justify-center rounded-full text-xs"><FaThumbsUp /></span>4.561
+                    <div className="relative" onMouseEnter={() => setShowLiked(true)} onMouseLeave={() => setShowLiked(false)}>
+                        {likeQuery.data && likeQuery.data.length > 0 && (
+                            <>
+                                <div className="flex gap-1 items-center">
+                                    <span className="bg-blue-600 w-6 h-6 text-white flex items-center justify-center rounded-full text-xs">
+                                        <FaThumbsUp />
+                                    </span>
+                                    <span>{likeQuery.data.length}</span>
+                                </div>
+                                {showLiked && (
+                                    <div className="absolute bg-white border flex flex-col p-2 rounded-md top-6">
+                                        {likeQuery.data.map((like) => {
+                                            return <span key={like.id}>{like.userName}</span>
+                                        })}
+                                    </div>
+                                )}
+                            </>
+                        )}
                     </div>
                     <button onClick={() => setShowComments(!showComments)}>
-                        {data && data.length > 0 ? `${data?.length} comentários` : ""}
+                        {commentQuery.data && commentQuery.data.length > 0 && `${commentQuery.data?.length} comentários`}
                     </button>
                 </div>
                 <div className="flex justify-around py-4 text-gray-600 border-b">
-                    <button className="flex items-center gap-1">
+                    <button onClick={() => shareLike()} className={`flex items-center ${liked ? 'text-blue-600' : ''}`}>
                         <FaThumbsUp /> Curtir
                     </button>
                     <button className="flex items-center gap-1" onClick={() => document.getElementById("comment" + id)?.focus()}>
                         <FaRegComment /> Comentar
                     </button>
                 </div>
-                {showComments && data?.map((comment, id) => {
+                {showComments && commentQuery.data?.map((comment, id) => {
                     return <Comment comment={comment} key={id} />
                 })}
                 <div className="flex gap-4 pt-6">

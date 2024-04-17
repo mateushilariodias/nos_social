2b4c49547b06ff295c1620e4e9a6a47bdcc8da96
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-04-17 17:32:01 -0300

    starts building user profile page

---
+++ client/nos_social/src/app/(authentificatedPages)/main/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Header from "@/components/HeaderFeed";
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../../../axios";
+import { ReactNode } from "react";
+
+export function MainUser({children}:{children:ReactNode}) {
+
+    const router = useRouter();
+
+    const { data, error, isSuccess, isError } = useQuery({
+        queryKey: ["refresh"],
+        queryFn: () => 
+            makeRequest.get("auth/refresh").then((res) => {
+                return res.data;
+            }),
+            retry: false,
+            refetchInterval: 60 * 60 * 1000,
+    });
+
+    if(isSuccess){
+        console.log(data.msg);
+        router.push('/loginUser')
+    }
+
+    if(isError){
+        console.log(error);
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-100">
+            <Header />
+            {children}
+        </main>
+    )
+}
---
+++ client/nos_social/src/app/(authentificatedPages)/profile/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../../../axios";
+
+function Profile({ searchParameters }: { searchParameters: { id: string } }) {
+
+    const { data, error } = useQuery({
+        queryKey: ['profile', searchParameters.id],
+        queryFn: () => makeRequest.get(`users/get-user?id=` + searchParameters.id).then((res) => {
+            return res.data[0]
+        })
+    })
+
+    if (error) {
+        console.log(error)
+    }
+    console.log(data)
+
+    return (
+        <div>{searchParameters.id}</div>
+    );
+}
+export default Profile;

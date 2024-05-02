edb0d650bec4a9e04e85a365a47049a7a7b6aa5d
Julio Cesar <julioafonso978@gmail.com>
2024-05-02 17:28:34 -0300

    initialization of token validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,14 +3,16 @@ import express from "express";
 
 // Importação das funções de registro e login do controlador de autenticação.
 import { registerUser, loginUser, refresh, logout, registerNgo } from "../controllers/auth.js";
+import { checkRefreshToken } from "../middleware/refreshTokenValidation.js";
+import { checkToken } from "../middleware/tokenValidation.js";
 
 // Criação de um objeto Router para definir rotas específicas.
 const router = express.Router();
 
 router.post("/registerUser", registerUser);
 router.post("/loginUser", loginUser);
-router.post("/logout", logout);
-router.get("/refresh", refresh);
+router.post("/logout", checkToken, logout);
+router.get("/refresh", checkRefreshToken, refresh);
 router.post("/registerNgo", registerNgo);
 
 export default router;
--- server/routes/post.js
+++ server/routes/post.js
@@ -3,16 +3,17 @@ import express from "express";
 
 // Importação dos controladores para manipular as rotas de postagem
 import { creatPost, getPost } from "../controllers/post.js";
+import { checkToken } from "../middleware/tokenValidation.js";
 
 // Criação de um novo objeto Router do Express
 const router = express.Router()
 
 // Definição das rotas para manipulação de postagens
 // Rota para criar uma nova postagem usando o método POST
-router.post("/", creatPost)
+router.post("/", checkToken, creatPost)
 
 // Rota para obter todas as postagens usando o método GET
-router.get("/", getPost)
+router.get("/", checkToken, getPost)
 
 // Exportação do objeto router para uso em outros arquivos
 export default router;

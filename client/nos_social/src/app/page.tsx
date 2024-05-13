d9af3779379a1ee6cd3c4547364be25ec53b9217
mateushilariodias <mateusilariodias@gmail.com>
2024-05-13 14:40:41 -0300

    start building profile editing logic

--- client/nos_social/src/app/page.tsx
+++ client/nos_social/src/app/page.tsx
@@ -1,4 +1,11 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+
+    const router = useRouter();
+    router.push('/homepage')
+
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         
--- client/nos_social/src/app/profile/page.tsx
+++ client/nos_social/src/app/profile/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../../axios";
 import Feed from "@/components/Feed";
 import { IPost } from "@/interfaces";
+import { useContext, useState } from "react";
+import { UserContext } from "@/context/userContext";
 
 function Profile({ searchParams }: { searchParams: { id: string } }) {
+
+    const queryClient = useQueryClient();
+    const { user } = useContext(UserContext);
+    const [userName, setUserName] = useState('');
+    const [userImg, setUserImg] = useState('');
+    const [bgImg, setBgImg] = useState('');
+    const [editProfile, setEditProfile] = useState(false);
+
 
     const profileQuery = useQuery({
         queryKey: ['profile', searchParams.id],
@@ -21,7 +31,7 @@ function Profile({ searchParams }: { searchParams: { id: string } }) {
     const postQuery = useQuery<IPost[] | undefined>({
         queryKey: ['posts/?id='],
         queryFn: () =>
-            makeRequest.get("post/"+searchParams.id).then((res) => {
+            makeRequest.get("post/" + searchParams.id).then((res) => {
                 return res.data.data
             })
     })
@@ -32,6 +42,17 @@ function Profile({ searchParams }: { searchParams: { id: string } }) {
         ]
     }
 
+    const editProfileMutation = useMutation({
+        mutationFn: async (data: { userName: string, userImg: string, bgImg: string, id: number }) => {
+            return makeRequest
+                .put(`users/update-user`, data)
+                .then((res) => res.data);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["profile", searchParams.id] })
+        },
+    });
+
     return (
         <div className="w-3/5 flex flex-col items-center">
             <div className="relative">
@@ -41,8 +62,18 @@ function Profile({ searchParams }: { searchParams: { id: string } }) {
                     <span className="text-2xl font-bold pl-2">{profileQuery.data?.author}</span>
                 </div>
             </div>
-            <div className="pt-36 w-3/5">
-                <Feed post={postQuery.data}/>
+            <div className="pt-36 w-3/5 flex flex-col items-center gap-4">
+                {user?.id === +searchParams.id ? (
+                    <span>Conheça mais do perfil.</span>
+                ) : 
+                <button className={`w-1/2 rounded-md py-2 font-semibold bg-zinc-300 hover:text-black`}>
+                    Editar perfil
+                </button>
+                }
+                {editProfile &&
+                    <div className="fixed top-0 bottom-0 right-0 left-0 bg-[#00000094] z-10 flex items-center justify-center">Editar perfil</div>
+                }
+                <Feed post={postQuery.data} />
             </div>
         </div>
     );
